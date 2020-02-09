c8aa870911dc92047cd2429776246fa169c020d5
Mauricio De Abreu <mauricioabreu0@gmail.com>
2020-02-09 11:59:01 -0500

    add !kick command

--- discord-interface/js/bot/index.js
+++ discord-interface/js/bot/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`)
-})
+});
 
 bot.on('message', msg => {
     if (msg.content === 'Hello') {
@@ -13,6 +13,30 @@ bot.on('message', msg => {
     if (msg.content === 'Hate') {
         msg.reply('Please be kind!');
     }
-})
+    if (msg.content.startsWith('!kick')) {
+        const member = msg.mentions.members.first()
+
+        if (!member) {
+            return msg.reply(
+                `Who are you trying to kick? You must mention a user.`
+            )
+        }
+
+        if (!member.kickable) {
+            return msg.reply(`I can't kick this user. Sorry!`)
+        }
+
+        return member
+            .kick()
+            .then(() => msg.reply(`${member.user.tag} was kicked.`))
+            .catch(error => msg.reply(`Sorry, an error occured.`))
+    }
+});
+
+bot.on('guildMemberAdd', member => {
+    member.send(
+        `Welcome on the server! Please be aware that we won't hate speech or harassment. Have fun 😀`
+    );
+});
 
 bot.login(process.env.BOT_TOKEN);
